73c2377194ef9480411f809dfc8bf4d2f87844b0
plainionist <stanley751@gmx.net>
2022-09-19 08:16:06 +0200

    Web host configuration simplified and CORS added

--- Athena/Athena.Web/Program.cs
+++ Athena/Athena.Web/Program.cs
@@ -1,31 +1,34 @@
+using Athena.Backlog.Adapters;
+using Athena.Backlog.UseCases;
+using Athena.Web.Fakes;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddSingleton(typeof(Athena.Backlog.Adapters.BacklogControllerAdapter));
-builder.Services.AddSingleton(typeof(Athena.Backlog.UseCases.BacklogInteractor));
-builder.Services.AddSingleton(typeof(Athena.Backlog.UseCases.IWorkItemRepository), typeof(Athena.Web.Fakes.FakeWorkItemRepository));
+builder.Services.AddSingleton(typeof(BacklogControllerAdapter));
+builder.Services.AddSingleton(typeof(BacklogInteractor));
+builder.Services.AddSingleton(typeof(IWorkItemRepository), typeof(FakeWorkItemRepository));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+})); 
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
+app.UseCors("corsapp");
 app.UseAuthorization();
 
 app.MapControllers();
