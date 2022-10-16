1878a12ff4dc6852851eed4fcfd66e46d9d6b5b5
plainionist <stanley751@gmx.net>
2022-10-16 10:20:26 +0200

    Agents: minor code cleanup

--- Agents/Orchestrator/AbstractAgent.cs
+++ Agents/Orchestrator/AbstractAgent.cs
@@ -3,95 +3,94 @@ using System.Collections.Generic;
 using System.Threading.Channels;
 using System.Threading.Tasks;
 
-namespace AboutCleanCode.Orchestrator
+namespace AboutCleanCode.Orchestrator;
+
+internal abstract class AbstractAgent : IAgent
 {
-    internal abstract class AbstractAgent : IAgent
+    private readonly Channel<Envelope> myQueue;
+    private readonly Dictionary<Type, Delegate> myMessageHandlers;
+    private Task myListeningTask;
+
+    internal AbstractAgent(ILogger logger)
     {
-        private readonly Channel<Envelope> myQueue;
-        private readonly Dictionary<Type, Delegate> myMessageHandlers;
-        private Task myListeningTask;
+        Logger = logger;
 
-        internal AbstractAgent(ILogger logger)
+        myQueue = Channel.CreateUnbounded<Envelope>();
+        myMessageHandlers = new Dictionary<Type, Delegate>();
+    }
+
+    protected void Receive<T>(Action<IAgent, T> handler)
+    {
+        if (myMessageHandlers.ContainsKey(typeof(T)))
         {
-            Logger = logger;
-
-            myQueue = Channel.CreateUnbounded<Envelope>();
-            myMessageHandlers = new Dictionary<Type, Delegate>();
+            throw new ArgumentException($"Handler already registered for messages of type: {typeof(T)}");
         }
 
-        protected void Receive<T>(Action<IAgent, T> handler)
+        myMessageHandlers[typeof(T)] = handler;
+    }
+
+    protected ILogger Logger { get; }
+
+    public void Post(IAgent sender, object message)
+    {
+        myQueue.Writer.WriteAsync(new Envelope
         {
-            if (myMessageHandlers.ContainsKey(typeof(T)))
+            Sender = sender,
+            Message = message
+        });
+    }
+
+    private class Envelope
+    {
+        public IAgent Sender { get; init; }
+        public object Message { get; init; }
+    }
+
+    public void Start()
+    {
+        myListeningTask = Listen();
+
+        Logger.Info(this, "started");
+    }
+
+    private async Task Listen()
+    {
+        await foreach (var envelope in myQueue.Reader.ReadAllAsync())
+        {
+            if (envelope.Message is PoisonPill)
             {
-                throw new ArgumentException($"Handler already registered for messages of type: {typeof(T)}");
+                Logger.Info(this, "stopped");
+                break;
             }
 
-            myMessageHandlers[typeof(T)] = handler;
-        }
-
-        protected ILogger Logger { get; }
-
-        public void Post(IAgent sender, object message)
-        {
-            myQueue.Writer.WriteAsync(new Envelope
+            try
             {
-                Sender = sender,
-                Message = message
-            });
-        }
-
-        private class Envelope
-        {
-            public IAgent Sender { get; init; }
-            public object Message { get; init; }
-        }
-
-        public void Start()
-        {
-            myListeningTask = Listen();
-
-            Logger.Info(this, "started");
-        }
-
-        private async Task Listen()
-        {
-            await foreach (var envelope in myQueue.Reader.ReadAllAsync())
+                OnReceive(envelope.Sender, envelope.Message);
+            }
+            catch (Exception ex)
             {
-                if (envelope.Message is PoisonPill)
-                {
-                    Logger.Info(this, "stopped");
-                    break;
-                }
-
-                try
-                {
-                    OnReceive(envelope.Sender, envelope.Message);
-                }
-                catch (Exception ex)
-                {
-                    Logger.Error(this, $"Failed to process '{envelope.Message.GetType().FullName}' from " +
-                        $"'{envelope.Sender.GetType().FullName}': {Environment.NewLine}{ex}");
-                }
+                Logger.Error(this, $"Failed to process '{envelope.Message.GetType().FullName}' from " +
+                    $"'{envelope.Sender.GetType().FullName}': {Environment.NewLine}{ex}");
             }
         }
+    }
 
-        protected virtual void OnReceive(IAgent sender, object message)
+    protected virtual void OnReceive(IAgent sender, object message)
+    {
+        if (myMessageHandlers.TryGetValue(message.GetType(), out var handler))
         {
-            if (myMessageHandlers.TryGetValue(message.GetType(), out var handler))
-            {
-                handler.DynamicInvoke(sender, message);
-            }
-            else
-            {
-                Logger.Warning(this, $"Unexpected message: {message.GetType()}");
-            }
+            handler.DynamicInvoke(sender, message);
         }
-
-        public void Stop()
+        else
         {
-            Post(this, new PoisonPill());
-
-            myListeningTask.Wait();
+            Logger.Warning(this, $"Unexpected message: {message.GetType()}");
         }
+    }
+
+    public void Stop()
+    {
+        Post(this, new PoisonPill());
+
+        myListeningTask.Wait();
     }
 }
