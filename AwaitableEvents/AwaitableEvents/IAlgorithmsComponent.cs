d1dcc2bdefe87a4b16dd712c139b4e2d6016d0c8
plainionist <stanley751@gmx.net>
2022-11-17 10:21:04 +0100

    AwaitableEvents: custom awaiter based on the Future object

--- AwaitableEvents/AwaitableEvents/IAlgorithmsComponent.cs
+++ AwaitableEvents/AwaitableEvents/IAlgorithmsComponent.cs
@@ -13,26 +13,51 @@ public interface IAlgorithmsComponent
 
 public static class AlgorithmsComponentExtensions
 {
-    public static TaskAwaiter<AlgorithmResult> GetAwaiter(this Future self)
-    {
-        var tcs = new TaskCompletionSource<AlgorithmResult>();
-
+    public static IAwaiter<AlgorithmResult> GetAwaiter(this Future self) =>
         // TODO: we should probably check whether result is already available
+        new AlgorithmsAwaiter(self);
 
-        void OnAlgorithmFinished(object _, AlgorithmFinishedEventArgs e)
+    private class AlgorithmsAwaiter : IAwaiter<AlgorithmResult>
+    {
+        private readonly IAlgorithmsComponent myComponent;
+        private readonly Guid myRequestId;
+        private AlgorithmResult myResult;
+        private Action myContinuation;
+
+        public AlgorithmsAwaiter(Future future)
         {
-            if (self.RequestId != e.RequestId)
+            myComponent = future.AlgorithmsComponent;
+            myComponent.AlgorithmFinished += OnAlgorithmFinished;
+
+            myRequestId = future.RequestId;
+        }
+
+        private void OnAlgorithmFinished(object sender, AlgorithmFinishedEventArgs e)
+        {
+            if (myRequestId != e.RequestId)
             {
                 return;
             }
 
-            self.AlgorithmsComponent.AlgorithmFinished -= OnAlgorithmFinished;
-            tcs.SetResult(e.Result);
+            myComponent.AlgorithmFinished -= OnAlgorithmFinished;
+            myResult = e.Result;
+
+            myContinuation();
         }
 
-        self.AlgorithmsComponent.AlgorithmFinished += OnAlgorithmFinished;
+        public bool IsCompleted => myResult != null;
 
-        return tcs.Task.GetAwaiter();
+        public AlgorithmResult GetResult() => myResult;
+
+        public void OnCompleted(Action continuation) =>
+            myContinuation = continuation;
     }
 }
+
+public interface IAwaiter<out TResult> : INotifyCompletion
+{
+    bool IsCompleted { get; }
+    TResult GetResult();
+}
+
 
