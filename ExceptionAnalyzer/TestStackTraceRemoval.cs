89ca081e3d04b03683969f1ef7e5cf807e1dd942
plainionist <mail@plainionist.net>
2023-01-30 13:37:24 +0100

    converted to file namespace

--- ExceptionAnalyzer/TestStackTraceRemoval.cs
+++ ExceptionAnalyzer/TestStackTraceRemoval.cs
@@ -1,58 +1,57 @@
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace ExceptionAnalyzer
+namespace ExceptionAnalyzer;
+
+public class TestStackTraceRemoval
 {
-    public class TestStackTraceRemoval
+    public bool IsSame(string data1, string data2)
     {
-        public bool IsSame(string data1, string data2)
+        var parser = new ExceptionParser();
+        var exception1 = parser.ExtractException(data1);
+        var exception2 = parser.ExtractException(data2);
+
+        if (exception1.Type == exception2.Type && exception1.Message == exception2.Message)
         {
-            var parser = new ExceptionParser();
-            var exception1 = parser.ExtractException(data1);
-            var exception2 = parser.ExtractException(data2);
+            var exceptionLines1 = exception1.StackTrace.Split('\n');
 
-            if (exception1.Type == exception2.Type && exception1.Message == exception2.Message)
+            var formattedException1 = new StringBuilder();
+
+            foreach (var line in exceptionLines1)
             {
-                var exceptionLines1 = exception1.StackTrace.Split('\n');
-
-                var formattedException1 = new StringBuilder();
-
-                foreach (var line in exceptionLines1)
+                if ((Regex.IsMatch(line, @"Tests\.Company\..*")
+                    || Regex.IsMatch(line, @"Company\.Product\..*(Integration|System)Tests\..*"))
+                    && (Regex.IsMatch(line, @"Tests\.")
+                    || Regex.IsMatch(line, @"HzTests\.")))
                 {
-                    if ((Regex.IsMatch(line, @"Tests\.Company\..*")
-                        || Regex.IsMatch(line, @"Company\.Product\..*(Integration|System)Tests\..*"))
-                        && (Regex.IsMatch(line, @"Tests\.")
-                        || Regex.IsMatch(line, @"HzTests\.")))
-                    {
-                        continue;
-                    }
-
-                    formattedException1.AppendLine(line);
+                    continue;
                 }
 
-                var formattedException2 = new StringBuilder();
-                var exceptionLines2 = exception2.StackTrace.Split('\n');
-
-                foreach (var line in exceptionLines2)
-                {
-                    if ((Regex.IsMatch(line, @"Tests\.Company\..*")
-                        || Regex.IsMatch(line, @"Company\.Product\..*(Integration|System)Tests\..*"))
-                        && (Regex.IsMatch(line, @"Tests\.")
-                        || Regex.IsMatch(line, @"HzTests\.")))
-                    {
-                        continue;
-                    }
-
-                    formattedException2.AppendLine(line);
-                }
-
-                if (formattedException1.ToString() == formattedException2.ToString())
-                {
-                    return true;
-                }
+                formattedException1.AppendLine(line);
             }
 
-            return false;
+            var formattedException2 = new StringBuilder();
+            var exceptionLines2 = exception2.StackTrace.Split('\n');
+
+            foreach (var line in exceptionLines2)
+            {
+                if ((Regex.IsMatch(line, @"Tests\.Company\..*")
+                    || Regex.IsMatch(line, @"Company\.Product\..*(Integration|System)Tests\..*"))
+                    && (Regex.IsMatch(line, @"Tests\.")
+                    || Regex.IsMatch(line, @"HzTests\.")))
+                {
+                    continue;
+                }
+
+                formattedException2.AppendLine(line);
+            }
+
+            if (formattedException1.ToString() == formattedException2.ToString())
+            {
+                return true;
+            }
         }
+
+        return false;
     }
 }
