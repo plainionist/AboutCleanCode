cfd5deda55747ffa544486d311be9d584af09297
plainionist <stanley751@gmx.net>
2023-02-13 08:38:01 +0100

    Athena.Controllers: code reformatted

--- Athena.Controllers/Backlog/Athena.Backlog.Adapters/BacklogVM.cs
+++ Athena.Controllers/Backlog/Athena.Backlog.Adapters/BacklogVM.cs
@@ -1,11 +1,10 @@
 using System.Collections.Generic;
 
-namespace Athena.Backlog.Adapters
+namespace Athena.Backlog.Adapters;
+
+public class BacklogVM
 {
-    public class BacklogVM
-    {
-        public IReadOnlyCollection<WorkItemVM> WorkItems { get; init; }
-        public string TotalEffort { get; init; }
-        public string TotalCapacity { get; init; }
-    }
+    public IReadOnlyCollection<WorkItemVM> WorkItems { get; init; }
+    public string TotalEffort { get; init; }
+    public string TotalCapacity { get; init; }
 }
--- Athena.Controllers/Backlog/Athena.Backlog.Adapters/WorkItemVM.cs
+++ Athena.Controllers/Backlog/Athena.Backlog.Adapters/WorkItemVM.cs
@@ -1,10 +1,9 @@
-namespace Athena.Backlog.Adapters
+namespace Athena.Backlog.Adapters;
+
+public class WorkItemVM
 {
-    public class WorkItemVM
-    {
-        public string Id { get; init; }
-        public string Title { get; init; }
-        public string AssignedTo { get; init; }
-        public string State { get; init; }
-    }
+    public string Id { get; init; }
+    public string Title { get; init; }
+    public string AssignedTo { get; init; }
+    public string State { get; init; }
 }
