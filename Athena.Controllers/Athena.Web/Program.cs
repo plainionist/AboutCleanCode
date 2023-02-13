7163145e87f1b018b3c4b8a801d553f65f0a75d8
plainionist <stanley751@gmx.net>
2023-02-13 10:22:11 +0100

    Athena.Controllers: Minimal Api version added

--- Athena.Controllers/Athena.Web/Program.cs
+++ Athena.Controllers/Athena.Web/Program.cs
@@ -6,28 +6,40 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(p => p.AddPolicy("CorsApp", builder =>
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader()));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// register application types 
 builder.Services.AddSingleton(typeof(BacklogControllerAdapter));
 builder.Services.AddSingleton(typeof(BacklogInteractor));
 builder.Services.AddSingleton(typeof(IWorkItemRepository), typeof(FakeWorkItemRepository));
 builder.Services.AddSingleton(typeof(ITeamsRepository), typeof(FakeTeamsRepository));
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+// register exception filter
 builder.Services.AddControllers(options =>
-{
-    options.Filters.Add<ControllerExceptionFilter>();
-});
-
-builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
-{
-    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
-})); 
+    options.Filters.Add<ControllerExceptionFilter>());
 
 var app = builder.Build();
+
+// define Web Api
+app.MapGet("/backlog/teams/{name}", (string name, string iteration, BacklogControllerAdapter adapter) =>
+    adapter.GetBacklog(name, iteration));
+
+
+
+
+
+
+
+
+
 
 if (app.Environment.IsDevelopment())
 {
@@ -35,9 +47,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors("corsapp");
-app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("CorsApp");
+app.UseAuthorization();
 
 app.Run();
