9eca0f2f0c95296fd7ec0c0e52db4cc744572c44
plainionist <stanley751@gmx.net>
2024-07-29 16:12:46 +0200

    Micro frontends: initial setup

--- MicroFrontend/App/src/App.vue
+++ MicroFrontend/App/src/App.vue
@@ -4,18 +4,11 @@
   <div>Welcome to the micro frontends demo</div>
 
   <div class="container">
-    <ResponsiveFrame :url="featureA" />
-    <ResponsiveFrame :url="featureB" />
+    <!-- TODO: integrate features here -->
   </div>
 </template>
 
 <script setup>
-  import { ResponsiveFrame } from 'library'
-
-  // In production we would retrieve these URIs from backend which 
-  // would probably read those from some configuration.
-  const featureA = 'http://localhost:7070'
-  const featureB = 'http://localhost:6060'
 </script>
 
 <style scoped>
