16f64557f8ada8d243fa6339b93d0e171236d77c
plainionist <stanley751@gmx.net>
2025-04-08 14:29:00 +0200

    TypeSafeWebApis: NSwag build integration improved

--- TypeSafeWebApis/NSwag-CodeFirst/WebUI/src/api/client.ts
+++ TypeSafeWebApis/NSwag-CodeFirst/WebUI/src/api/client.ts
@@ -15,7 +15,7 @@ export class Client {
 
     constructor(baseUrl?: string, http?: { fetch(url: RequestInfo, init?: RequestInit): Promise<Response> }) {
         this.http = http ? http : window as any;
-        this.baseUrl = baseUrl ?? "http://localhost:5101";
+        this.baseUrl = baseUrl ?? "";
     }
 
     getTodos(): Promise<TodoItem[]> {
