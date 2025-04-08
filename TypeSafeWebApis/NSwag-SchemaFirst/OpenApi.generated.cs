82c46c7ede46409de0916ab34d03aa1a20c5734a
plainionist <stanley751@gmx.net>
2025-04-08 06:16:19 +0200

    TypeSafeWebApis: code simplified

--- TypeSafeWebApis/NSwag-SchemaFirst/OpenApi.generated.cs
+++ TypeSafeWebApis/NSwag-SchemaFirst/OpenApi.generated.cs
@@ -4,8 +4,6 @@
 // </auto-generated>
 //----------------------
 
-using Microsoft.AspNetCore.Mvc;
-
 #pragma warning disable 108 // Disable "CS0108 '{derivedDto}.ToJson()' hides inherited member '{dtoBase}.ToJson()'. Use the new keyword if hiding was intended."
 #pragma warning disable 114 // Disable "CS0114 '{derivedDto}.RaisePropertyChanged(String)' hides inherited member 'dtoBase.RaisePropertyChanged(String)'. To make the current member override that implementation, add the override keyword. Otherwise add the new keyword."
 #pragma warning disable 472 // Disable "CS0472 The result of the expression is always 'false' since a value of type 'Int32' is never equal to 'null' of type 'Int32?'
@@ -38,7 +36,7 @@ namespace ToDo
 
     [System.CodeDom.Compiler.GeneratedCode("NSwag", "14.3.0.0 (NJsonSchema v11.2.0.0 (Newtonsoft.Json v13.0.0.0))")]
 
-    public partial class Controller : ControllerBase
+    public partial class Controller : Microsoft.AspNetCore.Mvc.ControllerBase
     {
         private IController _implementation;
 
